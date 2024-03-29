0d53209b00bbd5ca71f7b1426f63b452cd489685
Saoko1x <jemg2510@gmail.com>
2024-03-28 21:47:31 -0600

    classes

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
     static int id = 0; // Autoincrement
-    String name;
-    String email;
-    String speciality;
+    private String name;
+    private String email;
+    private String speciality;
+
 
     Doctor(){
         System.out.println("Construyendo el objeto doctor");
@@ -24,5 +28,48 @@ public class Doctor {
 
     public void showId(){
         System.out.println("ID Doctor: " + id);
+    }
+
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+    public void addAvailableAppointment(Date date, String time){
+        availableAppointments.add(new Doctor.AvailableAppointment(date, time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments(){
+        return availableAppointments;
+    }
+    public static class AvailableAppointment{
+        private int id;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
 
 //        showMenu();
         Doctor myDoctor = new Doctor("Jorge", "Oftalmologo");
-        System.out.println(myDoctor.name + " " +myDoctor.speciality);
 
         Patient patient = new Patient("Alejandra", "alejandra@mail.com");
         patient.setWeight(5);
